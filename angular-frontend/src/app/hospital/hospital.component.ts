555cb34a77751a34c4214a0a5c7679d02f0d8226
Rohan Martin <136007645+rohanxd1@users.noreply.github.com>
2025-05-18 13:48:29 +0530

    some errors in front end donor fixed

---
+++ angular-frontend/src/app/hospital/hospital.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hospital',
+  standalone: false,
+  templateUrl: './hospital.component.html',
+  styleUrl: './hospital.component.css'
+})
+export class HospitalComponent {
+
+}
